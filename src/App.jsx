1d0fe171c65ba1e918866322156d730cfcc0c19d
Faira73 <198441254+Faira73@users.noreply.github.com>
2025-12-08 13:47:32 +0300

    Add tracking route

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css';
 import { HomePage } from './pages/HomePage';
 import { OrdersPage} from './pages/OrdersPage';
 import { CheckOutPage } from './pages/checkout/CheckOutPage';
+import { TrackingPage } from './pages/TrackingPage';
 import { Routes, Route } from 'react-router';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Route index element={ <HomePage/> } />
       <Route path="/checkout" element={ <CheckOutPage/> } />
       <Route path="/orders" element={ <OrdersPage /> } />
+      <Route path="/tracking" element={ <TrackingPage /> }/>
     </Routes>
     
   )
